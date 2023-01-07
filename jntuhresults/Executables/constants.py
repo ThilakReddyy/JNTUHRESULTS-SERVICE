bcaced986aaa301fd29d4846468ec404e9fae97a
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2023-01-07 23:16:02 +0530

    Update constants.py

--- jntuhresults/Executables/constants.py
+++ jntuhresults/Executables/constants.py
@@ -12,14 +12,14 @@ url=["http://202.63.105.184/results/resultAction","http://results.jntuh.ac.in/re
 grades_to_gpa={'O':10,'A+':9,'A':8,'B+':7,'B':6,'C':5,'F':0,'Ab':0}
 
 #R18 Semester codes
-arr11=[1323,1358,1404,1430,1467,1504,1572]
-arr12=[1356,1363,1381,1435,1448,1481,1503,1570]
-arr21=[1391,1425,1449,1496,1560,1610]
-arr22=[1437,1447,1476,1501,1565,1605]
-arr31=[1454,1491,1550,1590]
-arr32=[1502,1555,1595]
-arr41=[1545,1585]
-arr42=[1580,1600]
+arr11=[1323,1358,1404,1430,1467,1504,1572,1615]
+arr12=[1356,1363,1381,1435,1448,1481,1503,1570,1620]
+arr21=[1391,1425,1449,1496,1560,1610,1628]
+arr22=[1437,1447,1476,1501,1565,1605,1627]
+arr31=[1454,1491,1550,1590,1626]
+arr32=[1502,1555,1595,1625]
+arr41=[1545,1585,1624]
+arr42=[1580,1600,1623]
 
 # payloads=["&etype=r17&result=null&grad=null&type=null&htno=","&etype=r17&result=gradercrv&grad=null&type=rcrvintgrade&htno="]
 payloads=["&etype=r17&result=null&grad=null&type=intgrade&htno=","&etype=r17&result=gradercrv&grad=null&type=rcrvintgrade&htno="]

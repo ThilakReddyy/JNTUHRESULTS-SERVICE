6df733f431d6734c3add3851e113155c406d392a
Thilak Reddy <thilakreddy19102000@gmail.com>
2023-05-21 16:29:43 +0530

    changing the view route

--- jntuhresults/urls.py
+++ jntuhresults/urls.py
@@ -3,6 +3,6 @@ from .views import multi,allResults,academicResult
 
 urlpatterns = [
     path('api/multi',multi.as_view()),
-    path('api/single',academicResult.as_view()),
+    path('api/single',allResults.as_view()),
     path('api/academicresult',academicResult.as_view())
 ]

62f1c9de94f78128cb463c87041251291abfb3bc
Thilak Reddy <thilakreddy19102000@gmail.com>
2023-08-02 18:47:33 +0530

    ADD:Test

--- jntuhresults/urls.py
+++ jntuhresults/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ClassResult,AcademicResult,Notification,homepage
+from .views import ClassResult,AcademicResult,Notification,homepage,test
 
 urlpatterns = [
     path('',homepage),
+    path('test',test),
     path('api/classresult',ClassResult.as_view()),
     path('api/academicresult',AcademicResult.as_view()),
     path('api/notifications',Notification.as_view())

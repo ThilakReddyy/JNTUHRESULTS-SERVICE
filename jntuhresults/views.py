4b8bf2f88d360a824b3b2293f7c9f399b1042538
thilakreddyy <thilakreddy19102000@gmail.com>
2024-02-09 19:48:12 +0530

    Add:priority

--- jntuhresults/views.py
+++ jntuhresults/views.py
@@ -86,13 +86,12 @@ class AcademicResult(View):
     def get(self, request):
         # Record the current time as the starting time
         starting = time.time()
-        url_index = 0
-        if check_url(0) is not True:
-            return HttpResponse(b"JNTUH Servers are down!!!", status=422)
-            if check_url(1) is not True:
+        url_index = 1
+        if check_url(1) is not True:
+            if check_url(0) is not True:
                 return HttpResponse(b"JNTUH Servers are down!!!", status=422)
             else:
-                url_index = 1
+                url_index = 0
 
         # Get the 'htno' parameter from the request and convert it to uppercase
         htno = request.GET.get("htno").upper()

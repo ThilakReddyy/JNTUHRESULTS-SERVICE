a8f0e0813d96edb9b64f19198e7f85684414ed10
thilakreddyy <thilakreddy19102000@gmail.com>
2024-02-13 13:14:25 +0530

    Nofication Retriver

--- jntuhresults/Executables/notificationsretriever.py
+++ jntuhresults/Executables/notificationsretriever.py
@@ -2,7 +2,7 @@ import requests
 import redis
 import json
 from bs4 import BeautifulSoup
-from datetime import timedelta
+from datetime import datetime, timedelta
 import os
 from dotenv import load_dotenv
 
@@ -19,24 +19,54 @@ def get_notifications():
     # if redis_response is not None:
     #    data = json.loads(redis_response)
     #    return data["data"]
-    url = "http://results.jntuh.ac.in/jsp/home.jsp"
-    # url="http://202.63.105.184/results/jsp/home.jsp"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    results = []
-    for i in range(0, 6):
-        jntuh_notifications = soup.find_all("table")[i].find_all("tr")
+    try:
+        url = "http://results.jntuh.ac.in/jsp/home.jsp"
+        # url="http://202.63.105.184/results/jsp/home.jsp"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        results = []
+        for i in range(0, 6):
+            jntuh_notifications = soup.find_all("table")[i].find_all("tr")
 
-        for result in jntuh_notifications:
-            result_link = result.find_all("td")[0].find_all("a")[0]["href"]
-            result_text = result.get_text()
-            result_text_index = result_text.find("Results") + 7
-            json_appendded = {
-                "Result_title": result_text[:result_text_index],
-                "Link": "http://results.jntuh.ac.in" + result_link,
-                "Date": result_text[result_text_index:],
-            }
-            results.append(json_appendded)
-    redis_client.set("notifications", json.dumps({"data": results}))
-    redis_client.expire("notifications", timedelta(minutes=120))
-    return results
+            for result in jntuh_notifications:
+                result_link = result.find_all("td")[0].find_all("a")[0]["href"]
+                result_text = result.get_text()
+                result_text_index = result_text.find("Results") + 7
+                json_appendded = {
+                    "Result_title": result_text[:result_text_index],
+                    "Link": "http://results.jntuh.ac.in" + result_link,
+                    "Date": result_text[result_text_index:],
+                }
+                results.append(json_appendded)
+        month_map = {
+            "JAN": 0,
+            "FEB": 1,
+            "MAR": 2,
+            "APR": 3,
+            "MAY": 4,
+            "JUN": 5,
+            "JUL": 6,
+            "AUG": 7,
+            "SEP": 8,
+            "OCT": 9,
+            "NOV": 10,
+            "DEC": 11,
+        }
+        new_results = []
+        for result in results:
+            try:
+                day, month, year = result["Date"].split("-")
+                month_abbreviation = str(month)[:3].upper()
+                month_index = month_map[month_abbreviation]
+                formatted_date = datetime(int(year), month_index + 1, int(day))
+                result["formatted_date"] = formatted_date.strftime("%Y-%m-%d")
+                new_results.append(result)
+            except Exception as e:
+                print(e)
+        results = new_results
+        results.sort(key=lambda x: x["formatted_date"], reverse=True)
+        redis_client.set("notifications", json.dumps({"data": results}))
+        redis_client.expire("notifications", timedelta(minutes=120))
+        return results
+    except Exception as e:
+        print(e)

6b3e231135eaffc750db7eae0ea79aa716d402c9
thilakreddyy <thilakreddy19102000@gmail.com>
2024-02-18 01:28:22 +0530

    rcrvretriever

---
+++ jntuhresults/Executables/rcrvretriever.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+response = requests.get("http://results.jntuh.ac.in/jsp/RCRVInfo.jsp")
+soup = BeautifulSoup(response.content, "html.parser")
+rcrvs = soup.find_all("h3")
+results = []
+for rcrv in rcrvs:
+    try:
+        splitedrcrv = rcrv.get_text().split(" ")[1:]
+
+        splitedone = ""
+        for splitted in splitedrcrv:
+            if "-20" in splitted:
+                splitedone = splitted[0:10]
+        result = {
+            "release data": rcrv.get_text().split(" ")[0].split("(")[1].split(")")[0],
+            "rcrv valuation": splitedone,
+        }
+
+        results.append(result)
+    except:
+        print(rcrv.get_text().split(" ")[1].split("(")[1].split(")")[0])
+print(results)

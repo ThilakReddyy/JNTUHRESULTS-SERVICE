654df94d2b0599f61d79b2b390f59dd44bda3fd9
thilakreddyy <thilakreddy19102000@gmail.com>
2025-01-21 12:35:54 +0530

    remove:

--- jntuhresults/views.py
+++ jntuhresults/views.py
@@ -202,7 +202,6 @@ class AcademicAllResults(View):
     def get(self, request):
         starting = time.time()
         htno = request.GET.get("htno").upper()
-        return HttpResponse(b"JNTUH Servers are down!!!", status=422)
         index = 1
         # if check_url(0, htno):
         #     index = 0
